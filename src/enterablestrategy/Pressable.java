b97fbd500cbaf1e0edd8999f3da407b058b6411b
MaciejZet <maciekzmitruk@protonmail.com>
2023-12-16 20:18:24 +0100

    Added ButtonEvent handling in Button class
    - Created ButtonEvent class to represent the button state
    - Extended Button class with observer mechanism
    - Implemented onEntered and onExited methods in Button class to generate ButtonEvent events

--- src/enterablestrategy/Pressable.java
+++ src/enterablestrategy/Pressable.java
@@ -6,41 +6,5 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Pressable extends Tile {
-    private final List<ButtonObserver> observers = new ArrayList<>();
-    private boolean isPressed = false;
 
-    public Button(int x, int y) {
-        super(x, y);
-        this.setEnterableStrategy(new Dummy());
-        this.setGraphicsID(Graphics.BUTTON_RELEASED);
-    }
-
-    public void addObserver(ButtonObserver observer) {
-        observers.add(observer);
-    }
-
-    public void removeObserver(ButtonObserver observer) {
-        observers.remove(observer);
-    }
-
-    private void notifyObservers() {
-        ButtonEvent event = new ButtonEvent(isPressed);
-        for (ButtonObserver observer : observers) {
-            observer.onButtonEvent(event);
-        }
-    }
-
-    @Override
-    public void onEntered(Direction direction, Tile tile, ActionTile actionTile) {
-        isPressed = true;
-        this.setGraphicsID(Graphics.BUTTON_PRESSED);
-        notifyObservers();
-    }
-
-    @Override
-    public void onExited(Direction direction, Tile tile) {
-        isPressed = false;
-        this.setGraphicsID(Graphics.BUTTON_RELEASED);
-        notifyObservers();
-    }
 }

aa9e92698bd568d14028b29a9d6d65074bb74454
emithedum <emilia@kosior.pl>
2024-01-08 23:53:59 +0100

    - move sending tilePressedEvents into a method to avoid code duplication - change all mouseClicked events to mousePressed

--- src/display/InteractiveTile.java
+++ src/display/InteractiveTile.java
@@ -43,17 +43,18 @@ public class InteractiveTile extends JPanel {
     public class EventListener extends MouseInputAdapter {
         @Override
         public void mousePressed(MouseEvent e) {
-            TilePressedEvent editorEvent = new TilePressedEvent(coords.x, coords.y, GameManager.getInstance().getEditor().getLayer());
-            //if (EditorDisplay.getMode() == EditorModes.ADD)
-            GameManager.getInstance().onEvent(editorEvent);
+            sendTilePressedEvent (e);
         }
         @Override
         public void mouseEntered(MouseEvent e) {
             if (SwingUtilities.isLeftMouseButton(e))
             {
-                TilePressedEvent editorEvent = new TilePressedEvent(coords.x, coords.y, GameManager.getInstance().getEditor().getLayer());
-                GameManager.getInstance().onEvent(editorEvent);
+                sendTilePressedEvent (e);
             }
+        }
+        public void sendTilePressedEvent (MouseEvent e) {
+            TilePressedEvent editorEvent = new TilePressedEvent(coords.x, coords.y, GameManager.getInstance().getEditor().getLayer());
+            GameManager.getInstance().onEvent(editorEvent);
         }
     }
     public void updateGraphics(enums.Graphics idBottom, enums.Graphics idUpper) {
